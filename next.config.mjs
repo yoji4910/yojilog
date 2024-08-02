f0d04d7cb213aaba173445b660a3b31ed009731b
yoji <fmn.4910@gmail.com>
2024-08-02 17:43:28 +0900

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    runtime: 'experimental-edge',
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
