662be6561451e1ac115d57912af2529c01303975
yoji <fmn.4910@gmail.com>
2024-08-03 17:06:41 +0900

    chnage settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracingRoot: './',
+  },
 };
 
 export default nextConfig;
