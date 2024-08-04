301d0eae83b7f08a266824b9ebd81d8f0dc764ee
yoji <fmn.4910@gmail.com>
2024-08-04 23:18:21 +0900

    add: tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Hello World! This is the Home page</p>
+      <p className="text-red-600">Hello World! This is the Home page</p>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
