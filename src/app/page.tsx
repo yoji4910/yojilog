b50760c3e9fc0f7dc2589a13c102d912b3170298
yoji <fmn.4910@gmail.com>
2024-08-04 20:08:40 +0900

    一旦prismicを極力排除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,48 @@
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
 
-import { SliceZone } from '@prismicio/react'
-import * as prismic from '@prismicio/client'
+// import { SliceZone } from '@prismicio/react'
+// import * as prismic from '@prismicio/client'
 
-import { createClient } from '@/prismicio'
-import { components } from '@/slices'
-import { PostCard } from '@/components/PostCard'
-import { Navigation } from '@/components/Navigation'
+// import { createClient } from '@/prismicio'
+// import { components } from '@/slices'
+// import { PostCard } from '@/components/PostCard'
+// import { Navigation } from '@/components/Navigation'
 
-export async function generateMetadata(): Promise<Metadata> {
-  const client = createClient()
-  const home = await client.getByUID('page', 'home')
+// export async function generateMetadata(): Promise<Metadata> {
+//   const client = createClient()
+//   const home = await client.getByUID('page', 'home')
 
-  return {
-    title: prismic.asText(home.data.title),
-    description: home.data.meta_description,
-    openGraph: {
-      title: home.data.meta_title || undefined,
-      images: [
-        {
-          url: home.data.meta_image.url || '',
-        },
-      ],
-    },
-  }
-}
+//   return {
+//     title: prismic.asText(home.data.title),
+//     description: home.data.meta_description,
+//     openGraph: {
+//       title: home.data.meta_title || undefined,
+//       images: [
+//         {
+//           url: home.data.meta_image.url || '',
+//         },
+//       ],
+//     },
+//   }
+// }
 
-export default async function Index() {
-  const client = createClient()
+export default async function Home() {
+  // const client = createClient()
 
-  const home = await client.getByUID('page', 'home')
+  // const home = await client.getByUID('page', 'home')
 
-  const posts = await client.getAllByType('blog_post', {
-    orderings: [
-      { field: 'my.blog_post.publication_date', direction: 'desc' },
-      { field: 'document.first_publication_date', direction: 'desc' },
-    ],
-  })
+  // const posts = await client.getAllByType('blog_post', {
+  //   orderings: [
+  //     { field: 'my.blog_post.publication_date', direction: 'desc' },
+  //     { field: 'document.first_publication_date', direction: 'desc' },
+  //   ],
+  // })
 
   return (
     <>
-      <Navigation client={client} />
+      <p>hello</p>
+      <p>this is home</p>
+      {/* <Navigation client={client} />
 
       <SliceZone slices={home.data.slices} components={components} />
 
@@ -50,7 +52,7 @@ export default async function Index() {
         ))}
       </section>
 
-      <Navigation client={client} />
+      <Navigation client={client} /> */}
     </>
   )
 }
