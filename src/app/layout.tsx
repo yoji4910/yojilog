b813938c7ca395c5f3b38084867568f2b4732552
yoji <fmn.4910@gmail.com>
2024-08-04 22:56:11 +0900

    一旦nextテンプレート

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,11 @@
-import '../styles/globals.css'
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="any"
-          href="https://prismic.io/favicon.ico"
-        />
-      </head>
-
-      <body className="flex flex-col items-center bg-stone-50">
-        <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
-          <ErrorBoundary fallback={<ErrorComponent />}>
-            <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
-              {children}
-            </div>
-          </ErrorBoundary>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
-  )
-}
-
-function ErrorBoundary({
-  children,
-  fallback,
-}: {
-  children: React.ReactNode
-  fallback: React.ReactNode
-}) {
-  try {
-    return children
-  } catch (error) {
-    console.error('Caught an error:', error)
-    return fallback
-  }
-}
-
-function ErrorComponent() {
-  return <div>Something went wrong. Please try again later.</div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,15 @@
-// import { Metadata } from 'next'
+import Link from "next/link";
 
-// import { SliceZone } from '@prismicio/react'
-// import * as prismic from '@prismicio/client'
-
-// import { createClient } from '@/prismicio'
-// import { components } from '@/slices'
-// import { PostCard } from '@/components/PostCard'
-// import { Navigation } from '@/components/Navigation'
-
-// export async function generateMetadata(): Promise<Metadata> {
-//   const client = createClient()
-//   const home = await client.getByUID('page', 'home')
-
-//   return {
-//     title: prismic.asText(home.data.title),
-//     description: home.data.meta_description,
-//     openGraph: {
-//       title: home.data.meta_title || undefined,
-//       images: [
-//         {
-//           url: home.data.meta_image.url || '',
-//         },
-//       ],
-//     },
-//   }
-// }
-
-export default async function Home() {
-  // const client = createClient()
-
-  // const home = await client.getByUID('page', 'home')
-
-  // const posts = await client.getAllByType('blog_post', {
-  //   orderings: [
-  //     { field: 'my.blog_post.publication_date', direction: 'desc' },
-  //     { field: 'document.first_publication_date', direction: 'desc' },
-  //   ],
-  // })
-
+const Home = () => {
   return (
-    <>
-      <p className="text-red-700">hello</p>
-      <p>this is home</p>
-      {/* <Navigation client={client} />
+    <div>
+      <h1>Home</h1>
+      <p>Hello World! This is the Home page</p>
+      <p>
+        Visit the <Link href="/about">About</Link> page.
+      </p>
+    </div>
+  );
+};
 
-      <SliceZone slices={home.data.slices} components={components} />
-
-      <section className="grid grid-cols-1 gap-8 max-w-3xl w-full">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
-      </section>
-
-      <Navigation client={client} /> */}
-    </>
-  )
-}
-
-// オプション: Incremental Static Regeneration (ISR) を有効にする
-// export const revalidate = 3600 // 1時間ごとに再生成
+export default Home;
