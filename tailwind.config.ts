03d148b35c9f82bdb315621bb6d088365d5f35c5
yoji <fmn.4910@gmail.com>
2024-08-05 18:09:18 +0900

    reset new nextjs app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
 import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
 
-export default {
-  content: ["./app/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-inter)"],
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [typography],
-} satisfies Config;
+  plugins: [],
+};
+export default config;
