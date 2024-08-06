d159148b58e456764d298ee33e56a0d7eea8b1bd
yoji <fmn.4910@gmail.com>
2024-08-06 16:46:50 +0900

    add H1 Link

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -1,27 +1,27 @@
-import Link from "next/link";
-import { Suspense } from "react";
+import Link from 'next/link'
+import { Suspense } from 'react'
 
-import Avatar from "./avatar";
-import CoverImage from "./cover-image";
-import DateComponent from "./date";
-import MoreStories from "./more-stories";
-import Onboarding from "./onboarding";
-import PortableText from "./portable-text";
+import Avatar from './avatar'
+import CoverImage from './cover-image'
+import DateComponent from './date'
+import MoreStories from './more-stories'
+import Onboarding from './onboarding'
+import PortableText from './portable-text'
 
-import type { HeroQueryResult, SettingsQueryResult } from "@/sanity.types";
-import * as demo from "@/sanity/lib/demo";
-import { sanityFetch } from "@/sanity/lib/fetch";
-import { heroQuery, settingsQuery } from "@/sanity/lib/queries";
+import type { HeroQueryResult, SettingsQueryResult } from '@/sanity.types'
+import * as demo from '@/sanity/lib/demo'
+import { sanityFetch } from '@/sanity/lib/fetch'
+import { heroQuery, settingsQuery } from '@/sanity/lib/queries'
 
 function Intro(props: { title: string | null | undefined; description: any }) {
-  const title = props.title || demo.title;
+  const title = props.title || demo.title
   const description = props.description?.length
     ? props.description
-    : demo.description;
+    : demo.description
   return (
     <section className="mt-16 mb-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
       <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
-        {title || demo.title}
+        <Link href="/">{title || demo.title}</Link>
       </h1>
       <h2 className="text-pretty mt-5 text-center text-lg lg:pl-8 lg:text-left">
         <PortableText
@@ -30,7 +30,7 @@ function Intro(props: { title: string | null | undefined; description: any }) {
         />
       </h2>
     </section>
-  );
+  )
 }
 
 function HeroPost({
@@ -42,7 +42,7 @@ function HeroPost({
   author,
 }: Pick<
   Exclude<HeroQueryResult, null>,
-  "title" | "coverImage" | "date" | "excerpt" | "author" | "slug"
+  'title' | 'coverImage' | 'date' | 'excerpt' | 'author' | 'slug'
 >) {
   return (
     <article>
@@ -70,7 +70,7 @@ function HeroPost({
         </div>
       </div>
     </article>
-  );
+  )
 }
 
 export default async function Page() {
@@ -79,7 +79,7 @@ export default async function Page() {
       query: settingsQuery,
     }),
     sanityFetch<HeroQueryResult>({ query: heroQuery }),
-  ]);
+  ])
 
   return (
     <div className="container mx-auto px-5">
@@ -107,5 +107,5 @@ export default async function Page() {
         </aside>
       )}
     </div>
-  );
+  )
 }
