65fb5df5e45f25a74cc2c01178ceac89d5b3fe55
yoji <fmn.4910@gmail.com>
2024-08-06 16:27:06 +0900

    remove comment

--- app/(blog)/portable-text.tsx
+++ app/(blog)/portable-text.tsx
@@ -1,13 +1,3 @@
-/**
- * This component uses Portable Text to render a post body.
- *
- * You can learn more about Portable Text on:
- * https://www.sanity.io/docs/block-content
- * https://github.com/portabletext/react-portabletext
- * https://portabletext.org/
- *
- */
-
 import {
   PortableText,
   type PortableTextComponents,
